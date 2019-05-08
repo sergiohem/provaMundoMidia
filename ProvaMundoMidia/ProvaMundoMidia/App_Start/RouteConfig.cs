aefbfe54980c1beb1a225406f04ffe7810c1ea59
sergiohenriquemarques <sergiohenriquemarques@outlook.com>
2019-05-07 21:46:55 -0300

    Criação da view "Index" para exibição da listagem de carros. Foram criadas partials para montar a tabela de carros, com o intuito de facilitar as chamadas Ajax.

--- ProvaMundoMidia/ProvaMundoMidia/App_Start/RouteConfig.cs
+++ ProvaMundoMidia/ProvaMundoMidia/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ProvaMundoMidia
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Carro", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ ProvaMundoMidia/ProvaMundoMidia/Controllers/CarroController.cs
@@ -0,0 +1,26 @@
+﻿using ProvaMundoMidia.DAOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProvaMundoMidia.Controllers
+{
+    public class CarroController : Controller
+    {
+        private CarroDAO carroDAO;
+
+        public CarroController()
+        {
+            carroDAO = new CarroDAO();
+        }
+
+
+        public ActionResult Index()
+        {
+            var carros = carroDAO.RetornarTodos();
+            return View(carros);
+        }
+    }
+}
